b04e2887200de098b1b92a566b3718f9cf7a43ba
unknown <tranquan.ttdt.usec@gmail.com>
2023-11-17 01:28:27 +0700

    done get api data blog

--- react-project-blog/src/conponents/BlogPage.jsx
+++ react-project-blog/src/conponents/BlogPage.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react"
+import BlogCard from "./BlogCard";
 
 
 export const BlogPage = () => {
+    const [blogs,setBlogs] = useState([]);
+    useEffect(()=> {
+        async function fetchBlogs() { 
+            let url = `http://localhost:5000/blogs`;
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            setBlogs(data);
+        }
+        fetchBlogs()
+    }, [])
   return (
-    <div>BlogPage</div>
+    <div>
+        {/* category section */}
+        <div>Page category</div>
+        {/* Blog card section */}
+        <div>
+            <BlogCard/>
+        </div>
+        {/* Pagination section */}
+        <div>Pagination</div>
+    </div>
   )
 }
