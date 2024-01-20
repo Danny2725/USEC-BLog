196b16d5d7e669b6764acfcf78e2cbe9f2c8aec4
Rookie Nguyễn <nguyenquocthang2004@gmail.com>
2024-01-20 20:30:28 +0700

    Types and stuffs

--- usec-blog-backend/db.js
+++ usec-blog-backend/db.js
@@ -6,14 +6,14 @@ const { MongoClient } = require("mongodb");
 let client = null;
 
 /**
+ * Connects to a MongoDB instance and calls the callback function after the connection is established
  *
  * @param {string} host Host of the MongoDB instance
  * @param {string | number | 27015} port Port of the MongoDB instance
  * @param {string} databaseName Database name to connect to
  * @param {string} username Username to connect to the database
  * @param {string} password Password to connect to the database
- * @param {} callback Callback function to be called after the connection is established
- * @returns {void}
+ * @param {function} callback Callback function to be called after the connection is established
  */
 async function connect(
     host,
@@ -47,10 +47,19 @@ async function connect(
     }
 }
 
+/**
+ * Connect to a different database in the same MongoDB instance
+ *
+ * @param {string} dbName The name of the database to connect to.
+ * @returns {import("mongodb").Db} The database object
+ */
 function db(dbName) {
     return client.db(dbName);
 }
 
+/**
+ * Closes the connection to the MongoDB instance
+ */
 function close() {
     if (client !== null) {
         client.close();
@@ -58,6 +67,16 @@ function close() {
     }
 }
 
+/**
+ * Generates a connection string for a MongoDB instance based on the parameters
+ *
+ * @param {string} host The host of the MongoDB instance
+ * @param {string | number} port The port of the MongoDB instance
+ * @param {string} databaseName The name of the database to connect to
+ * @param {string} username The username to connect to the database
+ * @param {string} password The password to connect to the database
+ * @returns string The MongoDB connection string
+ */
 function generateConnectionString(
     host,
     port,
